451bbcd26a4eed6ad562ba66f20b51fcddeb5781
asahisatou <88087478+asahisatou@users.noreply.github.com>
2022-03-01 16:19:09 +0900

    komitto

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main()
-{
-    std::cout << "あさひ" << std::endl;
-    return 0;
+int main() {
+ cv::Mat img;
+
+ img = cv::imread("/usr/local/pictures/chibany043.jpg");
+ cv::Mat gray;
+ cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+ cv::imshow("title", gray);
+ cv::waitKey(0);
+
+ return 0;
 }
